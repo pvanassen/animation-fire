6f7bd82f71fa1b5c60453d7db7467fb772ca9d89
Paul van Assen <source@pvanassen.nl>
2022-12-29 08:02:22 +0100

    Test using gif

--- src/main/kotlin/nl/pvanassen/led/animation/fire/Fire.kt
+++ src/main/kotlin/nl/pvanassen/led/animation/fire/Fire.kt
@@ -3,10 +3,12 @@ package nl.pvanassen.led.animation.fire
 import nl.pvanassen.led.animation.common.canvas.Canvas
 import nl.pvanassen.led.animation.common.model.Animation
 import nl.pvanassen.led.animation.common.util.CommonUtils
+import java.awt.Image
 import java.awt.image.BufferedImage
 import java.io.IOException
 import java.io.UncheckedIOException
 import javax.imageio.ImageIO
+import javax.swing.ImageIcon
 import kotlin.math.max
 import kotlin.math.min
 
@@ -14,10 +16,13 @@ class Fire(private val canvas: Canvas) : Animation<Any> {
 
     private val fireImage: BufferedImage
 
+    private val image: Image
+
     private var x: Int = 0
 
     init {
         try {
+            image = ImageIcon(javaClass.getResource("Animated_fire_by_nevit.gif")).image
             fireImage = ImageIO.read(javaClass.getResourceAsStream("/fire2.png"))
         } catch (e: IOException) {
             throw UncheckedIOException(e)
@@ -26,11 +31,12 @@ class Fire(private val canvas: Canvas) : Animation<Any> {
     }
 
     override fun getFrame(seed: Long, frame: Int, nsPerFrame: Int, helper: Any): ByteArray {
-        val moveX = CommonUtils.getRandom(SKEW * 2 + 1) - SKEW
-        x += moveX
-        x = max(0, x)
-        x = min(fireImage.width - canvas.getWidth(), x)
-        canvas.setImage(x, 0, fireImage)
+//        val moveX = CommonUtils.getRandom(SKEW * 2 + 1) - SKEW
+//        x += moveX
+//        x = max(0, x)
+//        x = min(fireImage.width - canvas.getWidth(), x)
+//        canvas.setImage(x, 0, fireImage)
+        canvas.drawImage(image)
         return canvas.getValues()
     }
 
