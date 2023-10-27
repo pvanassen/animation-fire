78975eae9d1503ed44e4af4d99a5debbbdd00731
Paul van Assen <source@pvanassen.nl>
2023-10-27 12:04:00 +0200

    1.7.0 - Version updates - Build update

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-DOCKER_BUILDKIT=1 docker build . -t animation-fire
+docker buildx build . --platform linux/amd64,linux/arm64 -t pvanassen.nl/led/animation-fire:latest --push --progress=plain
